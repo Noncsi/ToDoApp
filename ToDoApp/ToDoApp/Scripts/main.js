6ea7aa258927c872b29de21cbca9b177602776db
Sheiwa <lengyel.noemi96@gmail.com>
2017-09-27 14:26:42 +0200

    basic look done,
    lists are now saved in localStorage

---
+++ ToDoApp/ToDoApp/Scripts/main.js
@@ -0,0 +1,65 @@
+﻿var todolists = [];
+
+class TodoList {
+    constructor(title) {
+        this.title = title;
+        var tasks = [];
+        var opened = false;
+    }
+    addTask(task) {
+        tasks.push();
+    }               
+}
+
+$("#flash-message").hide();
+$(".opened-todo").hide();
+var todolist_id = 0;
+var task_id = 0;
+
+// load all list
+$(document).ready(function () {
+    for (var i = 1; i <= localStorage.length; i++) {
+        //var value = localStorage.getItem("task" + i);
+        //$("ul").append("<li id='task" + i + "'>" + value + "<span class='delete'>\u00D7</span></li>");
+        var title = localStorage.getItem("todolist" + i);
+        $(".todolist-container").append("<button class='todolist' id='todolist" + i + "'>" + title + "</button>");
+        todolist_id = i;
+    }
+})
+
+
+// add new list
+$("#add-title-btn").click(function () {
+    todolist_id++;
+    $(".todolist-container").append("<button class='todolist' id='todolist" + (todolist_id) + "'>" + $("#title-input").val() + "</button>");
+    localStorage.setItem("todolist" + todolist_id, $("#title-input").val());
+    $('#title-input').val() = "";
+    $('#new-todolist-title-input').hide();
+})
+
+
+function AddNewTask() {
+    if ($("#input").val() == "") { // show error message
+        $("#flash-message").slideDown(200, function () {
+            setTimeout(function () {
+                $("#flash-message").slideUp(200);
+            }, 3000);
+        });
+    } else { // add task and empty input
+        $("#list-of-tasks").append("<li id='task" + (++task_id) + "'>" + $("#input").val() + "<span class='delete'>\u00D7</span></li>");
+        localStorage.setItem("task" + task_id, $("#input").val());
+        $("#input").val("");
+    }
+}
+
+// delete task
+$("ul").on("click", ".delete", function () {
+    $(this.parentElement).remove();
+    localStorage.removeItem(this.parentElement.val());
+});
+
+// set the task status to completed
+$("ul").on("click", "li", function () {
+    $(this).toggleClass('completed-task');
+    //$(this).prepend('\u2713 ');   
+});
---
+++ ToDoApp/ToDoApp/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApp
+{
+    public class ToDoList
+    {
+        string title;
+        List<string> tasks = new List<string>();
+
+        public ToDoList(string title)
+        {
+            this.title = title;
+        }
+
+        public void AddTask(string task)
+        {
+            tasks.Add(task);
+        }
+
+        public List<string> GetTasks()
+        {
+            return tasks;
+        }
+    }
+}
